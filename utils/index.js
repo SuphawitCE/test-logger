2940f80a3630aa494faa4395fe525452324b8b3d
SuphawitCE <50129987+SuphawitCE@users.noreply.github.com>
2021-11-04 09:58:44 +0700

    refactor(keys): [PFMT-5668] Refactor firebaseUserResponse keys

--- utils/index.js
+++ utils/index.js
@@ -1,38 +1,3 @@
-// exports.SRPALogin = [
-//   '*.request.userAttributes["custom:customer_id"]',
-//   '*.request.userAttributes["cognito:email_alias"]',
-//   '*.request.userAttributes["custom:firebase_uid"]',
-//   "*.request.userAttributes.given_name",
-//   "*.request.userAttributes.family_name",
-//   "*.request.userAttributes.email",
-// ];
-
-// exports.PhoneLogin = [
-//   '*.request.userAttributes["custom:customer_id"]',
-//   '*.request.userAttributes["cognito:email_alias"]',
-//   '*.request.userAttributes["custom:firebase_uid"]',
-//   '*.request.userAttributes["cognito:phone_number_alias"]',
-//   "*.request.userAttributes.given_name",
-//   "*.request.userAttributes.phone_number",
-//   "*.request.userAttributes.family_name",
-//   "*.request.userAttributes.email",
-// ];
-
-const integrateString = () => {
-  const text = "firebaseUserResponse.";
-  const providerData = [
-    "providerData[*].uid",
-    "providerData[*].displayName",
-    "providerData[*].email",
-    "providerData[*].photoURL",
-  ];
-  const temp = [];
-  providerData.forEach((providerKey) => temp.push(`${text}${providerKey}`));
-  return temp;
-};
-
-const firebaseUserResponse = integrateString();
-
 exports.cognitoKeys = [
   '*.request.userAttributes["custom:customer_id"]',
   '*.request.userAttributes["cognito:email_alias"]',
@@ -47,9 +12,8 @@ exports.cognitoKeys = [
   "*.domain",
   "*.displayName",
   "*.photoURL",
-  ...firebaseUserResponse,
-  // "firebaseUserResponse.providerData[*].uid",
-  // "firebaseUserResponse.providerData[*].displayName",
-  // "firebaseUserResponse.providerData[*].email",
-  // "firebaseUserResponse.providerData[*].photoURL",
+  'firebaseUserResponse.providerData[*].uid',
+  'firebaseUserResponse.providerData[*].displayName',
+  'firebaseUserResponse.providerData[*].email',
+  'firebaseUserResponse.providerData[*].photoURL'
 ];
